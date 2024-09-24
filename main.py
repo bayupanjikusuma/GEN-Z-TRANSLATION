e490debd3fa80e173ab2145a53de5e3fcde054bd
bayupanjikusuma <panjikusumabayu@gmail.com>
2024-09-24 19:20:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,55 @@
-meme_dict = {"CRINGE": "Sesuatu yang sangat aneh atau memalukan",
-            "LOL": "Tanggapan umum terhadap sesuatu yang lucu", 'ROFL': 'tanggapan terhadap lelucon', 'SHEESH': 'sedikit ketidak setujuan',
-            'CREEPY':  'menakutkan, tidak menyenangkan'}
-word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print('mohon maaf kata kata tidak tersedia')
+import discord
+import random
+import asyncio
+
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.event
+async def on_member_join(member):
+        guild = member.guild
+        if guild.system_channel is not None:
+            to_send = f'Welcome {member.mention} to {guild.name}!'
+            await guild.system_channel.send(to_send)
+
+@bot.event
+async def on_message(message):
+        # we do not want the bot to reply to itself
+        if message.author.id == bot.user.id:
+            return
+
+        if message.content.startswith('$guess'):
+            await message.channel.send('Guess a number between 1 and 10.')
+
+            def is_correct(m):
+                return m.author == message.author and m.content.isdigit()
+
+            answer = random.randint(1, 10)
+
+            try:
+                guess = await bot.wait_for('message', check=is_correct, timeout=5.0)
+            except asyncio.TimeoutError:
+                return await message.channel.send(f'Sorry, you took too long it was {answer}.')
+
+            if int(guess.content) == answer:
+                await message.channel.send('You are right!')
+            else:
+                await message.channel.send(f'Oops. It is actually {answer}.')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("")
